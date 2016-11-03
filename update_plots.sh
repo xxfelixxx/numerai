e7e5babe2824ff1a6e5b6b2da6ef24ef848e4fe7
Felix Tubiana <felixtubiana@gmail.com>
2016-11-03 19:30:30 +0800

    Update summary plots

--- update_plots.sh
+++ update_plots.sh
@@ -10,10 +10,14 @@ cd html
 echo 'Updating table.html'
 perl create_html.pl
 
+echo 'Updating summary.html'
+perl create_summary_html.pl
+
 echo 'Adding latest plots to git'
 git add *.html *.png
 git commit -m 'Updated plots with the latest numerai data.'
 DIR=`pwd`
 HTML="file://$DIR/index.html"
+SUMM="file://$DIR/summary.html"
 echo 
 echo "Latest plots are visible at: $HTML"
